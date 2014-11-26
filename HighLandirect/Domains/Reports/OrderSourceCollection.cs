7c3864143e8c8968bcd3d19db8029f83414cd379
koty <kouichi.nishizawa+github@gmail.com>
2014-11-26 15:29:33 +0900

    大口用の送り状帳票を追加

---
+++ HighLandirect/Domains/Reports/OrderSourceCollection.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HighLandirect.Domains.Reports
+{
+    public class OrderSourceCollection
+    {
+        public IEnumerable<OrderSource> Orders { get; private set; }
+        public int StartAt { get; private set; }
+
+        public OrderSourceCollection()
+        {
+            if (DesignerProperties.GetIsInDesignMode(new DependencyObject()))
+            {
+                var list = new List<OrderSource>();
+                for(var i = 0 ; i < 10; i++)
+                {
+                    list.Add(new OrderSource());
+                }
+                this.Orders = list;
+            }
+        }
+
+        public OrderSourceCollection(IEnumerable<OrderSource> orders, int startAt)
+        {
+            this.Orders = orders;
+            this.StartAt = startAt;
+        }
+    }
+}
